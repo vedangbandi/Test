74c6a5fb4c90bb6e2330aa65f25cc4f112098295
Vedang Bandi <96762504+vedangbandi@users.noreply.github.com>
2024-01-12 16:36:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return def star(n):
-    for i in range(0, n):
-        for j in range(0, i+1):
-            print("* ",end="")
-        print("\r")
-n = 5
-star(n)
+                for i in range(0, n):
+                           for j in range(0, i+1):
+                                      print("* ",end="")
+                           print("\r")
+ n = 5
+ star(n)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000)
